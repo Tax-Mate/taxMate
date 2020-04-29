4d5e904ee73b21552ccde05d95cb5a77dd0e4705
Ubuntu <ubuntu@ip-172-31-22-119.us-east-2.compute.internal>
2020-04-29 01:13:23 +0000

    Added views and controllers for State Tax and Resources Again

--- application/app/helpers/stateforms_helper.rb
+++ application/app/helpers/stateforms_helper.rb
@@ -56,7 +56,27 @@ def calculate (income, status, state )
         end
     
     else if @status == "joint"
-        return 456
+         case @stateform.state
+           when "NC"  
+               
+                 return  @total = (@stateform.income * 0.129)*2 
+           when "AL"
+               if @income <3000
+                   return  @total = (@stateform.income * 0.02)*2
+               elsif @income >= 3000
+                return  @total = (@stateform.income * 0.05)*2
+                    
+               end
+           
+           when "AK"
+           return  @total = @stateform.income
+           when "AZ"
+               if@income <=26000
+                   return  @total = (@stateform.income * 0.0259) 
+               elsif @income >26501||@income >= 53000
+                return  @total = (@stateform.income * 0.0334)
+               end
+                    
     end
 
   
